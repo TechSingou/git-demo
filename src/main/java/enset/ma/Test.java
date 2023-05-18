996982400e880672a8a1f925edec966b5bbdd5b4
techSingou <techsingou@gmail.com>
2023-05-18 21:50:56 +0100

    Comments

--- src/main/java/enset/ma/Test.java
+++ src/main/java/enset/ma/Test.java
@@ -1,7 +1,10 @@
+/** Comment */
+
 package enset.ma;
 
 public class Test{
     public static void main(String[] args) {
+        // ------------------------------
         System.out.println("Hello world");
     }
 }
